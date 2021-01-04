89b034bcf34872fd843db5c5f12859c64a1be292
Constantine <faustlarsen@gmail.com>
2021-01-04 12:03:56 -0700

    add student index and details
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- Registrar/Controllers/StudentsController.cs
+++ Registrar/Controllers/StudentsController.cs
@@ -1,14 +1,33 @@
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Registrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace Registrar.Controllers
-// {
-//   public class StudentsController : Controller
-//   {
-//     private readonly RegistrarContext _db;
+namespace Registrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly RegistrarContext _db;
 
-//     public StudentsController(RegistrarContext db)
-//     {
-//       _db = db;
-//     }
-//   }
-// }
+    public StudentsController(RegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+        return View(_db.Students.ToList());
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisStudent = _db.Students
+        .Include(student => student.Courses)
+        .ThenInclude(join=>join.Course)
+        .FirstOrDefault(student=>student.StudentId==id);
+      return View(thisStudent);
+    }
+  }
+}
