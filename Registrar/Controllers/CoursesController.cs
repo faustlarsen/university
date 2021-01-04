11cf8e8ce2c4e595d98ac7ee02c795eba9a0bb82
Constantine <faustlarsen@gmail.com>
2021-01-04 16:06:38 -0700

    add courses details, index,create
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- Registrar/Controllers/CoursesController.cs
+++ Registrar/Controllers/CoursesController.cs
@@ -1,15 +1,46 @@
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Registrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-// namespace Registrar.Controllers
-// {
-//   public class CoursesController : Controller
-//   {
-//     private readonly RegistrarContext _db;
+namespace Registrar.Controllers
+{
+  public class CoursesController : Controller
+  {
+    private readonly RegistrarContext _db;
 
-//     public CoursesController(RegistrarContext db)
-//     {
-//       _db = db;
-//     }
+    public CoursesController(RegistrarContext db)
+    {
+      _db = db;
+    }
 
-//   }
-// }
+    public ActionResult Index()
+    {
+      List<Course> model = _db.Courses.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisCourse = _db.Courses
+        .Include(course => course.Students)
+        .ThenInclude(join => join.Student)
+        .FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+  }
+}
--- Registrar/Models/Student.cs
+++ Registrar/Models/Student.cs
@@ -3,36 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-//using System.Windows;
-//using System.ComponentModel.Design;
-
-//using System.DateTime.Date;
 
 namespace Registrar.Models
 {
-  public class Student
-  {
-    public Student()
+    public class Student
     {
-        this.Courses = new HashSet<CourseStudent>();
+        public Student()
+        {
+            this.Courses = new HashSet<CourseStudent>();
+        }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        [DisplayName("DateOfEnrollment")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime DateOfEnrollement { get; set; }
+        public ICollection<CourseStudent> Courses { get; }
     }
-
-    public int StudentId { get; set; }
-    public string StudentName { get; set; }
-    //public class DisplayNameAttribute : Attribute
-    //{
-    [DisplayName("DateOfEnrollment")]
-    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
-    
-    public DateTime DateOfEnrollement { get; set; }
-    //}
-    public ICollection<CourseStudent> Courses { get; }
-
-  }
 }
-
-// DateTime date1 = new DateTime(2008, 6, 1);
-//   // Get date-only portion of date, without its time.
-//   DateTime dateOnly = date1.Date;
-//   // Display date using short date string.
-//   Console.WriteLine(dateOnly.ToString("d"));
