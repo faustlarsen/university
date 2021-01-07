1addb5eb3d01e26e8eca06b7878bd47c94b76865
Constantine <faustlarsen@gmail.com>
2021-01-07 15:38:13 -0700

    add courses to department

--- Registrar/Controllers/DepartmentsController.cs
+++ Registrar/Controllers/DepartmentsController.cs
@@ -39,6 +39,8 @@ namespace Registrar.Controllers
     public ActionResult Details(int id)
     {
         Department thisDepartment = _db.Departments.FirstOrDefault(department => department.DepartmentId == id);
+        List<Course> foundCourses = _db.Courses.Where(course => course.DepartmentId == id).ToList();
+        ViewBag.foundCourses = foundCourses;
         return View(thisDepartment);
     }
 
@@ -70,7 +72,7 @@ namespace Registrar.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
-    public ActionResult AddCourse(int id)   
+    public ActionResult AddCourse(int id)
     {
         var thisDepartment = _db.Departments.FirstOrDefault(departments => departments.DepartmentId == id);
         ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
@@ -79,9 +81,51 @@ namespace Registrar.Controllers
     [HttpPost]
     public ActionResult AddCourse(Course course, int DepartmentId)
     {
-        _db.Courses.Add(new Course() { CourseId = course.CourseId, DepartmentId = DepartmentId });   
+        var thisCourse = _db.Courses.FirstOrDefault(element => element.CourseId == course.CourseId);
+        thisCourse.DepartmentId = DepartmentId; //line 83 is true/happened
+        _db.Courses.Attach(thisCourse); // prepping for a save
+        _db.Entry(thisCourse).Property(element => element.DepartmentId).IsModified = true; // edit modification to db from line 83
+        // _db.Entry(thisCourse).State = EntityState.Modified; //not needed
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
+
+    // [HttpPost]
+    // public ActionResult Edit(Item item)
+    // {
+    //   _db.Entry(item).State = EntityState.Modified;
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+
+    // [HttpPost]
+    // public ActionResult Create(Student student, int CourseId)
+    // {
+    //     _db.Students.Add(student);
+    //     if (CourseId != 0)
+    //     {
+              // var thisCourseStudent = _db.CourseStudent.FirstOrDefault(courseStudent => courseStudent.CourseId == CourseId); // needs another condition to check if it has a student associated
+              // if (thisCourse )
+              // {
+              //   _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId});
+              // } else {
+              //   ViewBag.CourseStudentExists = "That course already belongs to this student.";
+              //   return View("CourseStudentExists")
+              //
+    //     }
+    //     _db.SaveChanges();
+    //     return RedirectToAction("Index");
+    // }
+
+    // In your view:
+    // @{
+    //   Timer timer = new Timer();
+    //   timer.interval = 1000; //1sec
+    //   time.Start();
+    //   while(timer > 5000)
+    //   {
+    //     RedirectAction
+    //   }
+    //  }
   }
 }
